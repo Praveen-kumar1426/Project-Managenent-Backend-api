f6894ecca5d6e0ec6d85a979aebebcc9e33a9683
Praveen-kumar1426 <pk5377300@gmail.com>
2026-03-30 11:57:54 +0530

    add create project and add project member validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,5 +1,5 @@
 import { body } from "express-validator";
-
+import {AvailableUserRole} from "../utils/constant.js"
 const userRegisterValidator =()=>{
    return [
      body("email")
@@ -53,4 +53,18 @@ const userResetForgotPasswordValidator=()=>{
     body("newPassword").notEmpty().withMessage("Password is required")
   ]
 }
-export{userRegisterValidator, userLoginValidator, userChangesCurrentPasswordValidator, userForgotPasswordValidator,userResetForgotPasswordValidator}
+
+const createProjectValidator = ()=>{
+  return [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("description").optional()
+  ]
+}
+
+const addMemberProjectValidator = ()=>{
+  return [
+    body("email").trim().notEmpty().withMessage("Email is required").isEmail().withMessage("Email is invalid"),
+    body("role").notEmpty().withMessage("Role is required").isIn(AvailableUserRole).withMessage("Role is invalid")
+  ]
+}
+export{userRegisterValidator, userLoginValidator, userChangesCurrentPasswordValidator, userForgotPasswordValidator,userResetForgotPasswordValidator, createProjectValidator, addMemberProjectValidator}
